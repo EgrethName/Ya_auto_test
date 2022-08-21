0f35b13e1498ee0f04b8333a142f032c520ca09b
Anna Skorykh <egreth@mail.ru>
2022-08-21 16:04:05 +0300

    refactoring for courier's data generation

--- test_create_courier.py
+++ test_create_courier.py
@@ -1,28 +1,45 @@
 import pytest
+import random
 import requests
 
-from data import Links, CourierLoginData
+from data import Links
+
+
+def generate_random_string(string_length):
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    random_string = ''.join(random.choice(letters) for i in range(string_length))
+    return random_string
+
+
+def create_login_data(login):
+    login_data = {
+        "login": login,
+        "password": "1234",
+        "firstName": "frodo"
+    }
+    return login_data
 
 
 class TestForCreateCourier:
 
     test_data = [
-        pytest.param(CourierLoginData.SUCCESSFUL_LOGIN, 201, id="test_successful_courier_create"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_2, 201, id="test_login_length_2"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_1, 400, id="test_login_length_1"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_3, 201, id="test_login_length_3"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_9, 201, id="test_login_length_9"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_10, 201, id="test_login_length_10"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_11, 400, id="test_login_length_11"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_255, 400, id="test_login_length_255"),
-        pytest.param(CourierLoginData.LOGIN_LENGTH_256, 400, id="test_login_length_256"),
-        pytest.param(CourierLoginData.CYRILLIC_LOGIN, 400, id="test_cyrillic_login"),
-        pytest.param(CourierLoginData.SPEC_SYMBOLS_LOGIN, 400, id="test_spec_symbols_login"),
-        pytest.param(CourierLoginData.DIGITAL_STR_LOGIN, 400, id="test_digital_str_login"),
-        pytest.param(CourierLoginData.DIGITAL_INT_LOGIN, 400, id="test_digital_int_login"),
-        pytest.param(CourierLoginData.LOGIN_NONE, 400, id="test_login_none"),
-        pytest.param(CourierLoginData.LOGIN_TRUE, 400, id="test_login_true"),
-        pytest.param(CourierLoginData.LOGIN_WITH_SPACES, 400, id="test_login_with_spaces")
+        pytest.param(create_login_data(generate_random_string(7)), 201, id="test_successful_courier_create"),
+        pytest.param(create_login_data(generate_random_string(2)), 201, id="test_login_length_2"),
+        pytest.param(create_login_data(generate_random_string(1)), 400, id="test_login_length_1"),
+        pytest.param(create_login_data(generate_random_string(3)), 201, id="test_login_length_3"),
+        pytest.param(create_login_data(generate_random_string(9)), 201, id="test_login_length_9"),
+        pytest.param(create_login_data(generate_random_string(10)), 201, id="test_login_length_10"),
+        pytest.param(create_login_data(generate_random_string(11)), 400, id="test_login_length_11"),
+        pytest.param(create_login_data(generate_random_string(255)), 400, id="test_login_length_255"),
+        pytest.param(create_login_data(generate_random_string(256)), 400, id="test_login_length_256"),
+        pytest.param(create_login_data("фродо"), 400, id="test_cyrillic_login"),
+        pytest.param(create_login_data("!№;%:\""), 400, id="test_spec_symbols_login"),
+        pytest.param(create_login_data("56789"), 400, id="test_digital_str_login"),
+        pytest.param(create_login_data(56789), 400, id="test_digital_int_login"),
+        pytest.param(create_login_data(None), 400, id="test_login_none"),
+        pytest.param(create_login_data(True), 400, id="test_login_true"),
+        pytest.param(create_login_data(False), 400, id="test_login_false"),
+        pytest.param(create_login_data("   "), 400, id="test_login_with_spaces")
     ]
 
     @pytest.mark.parametrize("payload,expected_status_code", test_data)
